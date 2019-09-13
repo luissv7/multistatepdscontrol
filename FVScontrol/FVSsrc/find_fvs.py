8521bbedd45eff783db163568105dd021f4d8505
Luis Sordo Vieira <luis.sordovieira@jax.org>
2019-09-13 10:11:29 -0400

    Structured file better

--- FVScontrol/FVSsrc/find_fvs.py
+++ FVScontrol/FVSsrc/find_fvs.py
@@ -1,7 +1,7 @@
 import networkx;
 import os,sys,FVS;
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
-with open ('bladder_tumorogenesis.sif') as f:
+with open ('network/bladder_tumorogenesis.sif') as f:
 	edges=f.readlines()
 data=[tuple(line.strip().split()) for line in edges]
 data=[(edge[0],edge[2]) for edge in data]
@@ -11,6 +11,6 @@ G = networkx.DiGraph()
 G.add_edges_from(data)
 G_FVS=FVS.FVS(G, T_0=0.6, alpha=0.99, maxMvt_factor=5, maxFail=50, randomseed=1)
 
-with open('results.txt', 'w') as writer:
+with open('results/results.txt', 'w') as writer:
 	for item in G_FVS:
         	writer.write("%s\n" % item)
